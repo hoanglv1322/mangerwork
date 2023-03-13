4743218261a375af1a242836d103a1b6df4fed61
hoanglv1322 <87842327+hoanglv1322@users.noreply.github.com>
2023-03-13 22:38:11 +0700

    Update constants.js

--- client/src/context/constants.js
+++ client/src/context/constants.js
@@ -1,6 +1,6 @@
 export const apiUrl =
-	process.env.NODE_ENV !== 'production'
-		? 'https://manager-work.onrender.com/api'
-		: 'something'
+	process.env.NODE_ENV 
+		'https://manager-work.onrender.com/api'
+		
 
 export const LOCAL_STORAGE_TOKEN_NAME = 'manager-work-app'
